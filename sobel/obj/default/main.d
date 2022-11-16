91466e06254ac680a29294a776ad1e515ae94ea5
schnicPV <sch.ncls@outlook.com>
2022-11-16 18:35:41 +0100

    undesired files to ignore (not working)

--- sobel/obj/default/main.d
+++ sobel/obj/default/main.d
@@ -3,7 +3,14 @@ obj/default/main.o: main.c \
  C:/Users/nicis/HESSO/MSc_ElE/TSM_EmbHardw/Labs/Lab_04/mse_demo/software/sobel_bsp/linker.h \
  c:\users\nicis\hesso\msc_ele\tsm_embhardw\labs\lab_04\mse_demo\software\sobel_bsp\hal\inc\io.h \
  c:\users\nicis\hesso\msc_ele\tsm_embhardw\labs\lab_04\mse_demo\software\sobel_bsp\hal\inc\alt_types.h \
- lcd_simple.h grayscale.h i2c.h camera.h vga.h dipswitch.h sobel.h
+ lcd_simple.h grayscale.h i2c.h camera.h vga.h dipswitch.h sobel.h \
+ c:\users\nicis\hesso\msc_ele\tsm_embhardw\labs\lab_04\mse_demo\software\sobel_bsp\hal\inc\sys\alt_timestamp.h \
+ c:\users\nicis\hesso\msc_ele\tsm_embhardw\labs\lab_04\mse_demo\software\sobel_bsp\hal\inc\alt_types.h \
+ c:\users\nicis\hesso\msc_ele\tsm_embhardw\labs\lab_04\mse_demo\software\sobel_bsp\drivers\inc\altera_avalon_timer.h \
+ c:\users\nicis\hesso\msc_ele\tsm_embhardw\labs\lab_04\mse_demo\software\sobel_bsp\hal\inc\sys\alt_dev.h \
+ c:\users\nicis\hesso\msc_ele\tsm_embhardw\labs\lab_04\mse_demo\software\sobel_bsp\hal\inc\sys\alt_llist.h \
+ c:\users\nicis\hesso\msc_ele\tsm_embhardw\labs\lab_04\mse_demo\software\sobel_bsp\hal\inc\priv\alt_dev_llist.h \
+ c:\users\nicis\hesso\msc_ele\tsm_embhardw\labs\lab_04\mse_demo\software\sobel_bsp\hal\inc\sys\alt_warning.h
 
 C:/Users/nicis/HESSO/MSc_ElE/TSM_EmbHardw/Labs/Lab_04/mse_demo/software/sobel_bsp/system.h:
 
@@ -26,3 +33,17 @@ vga.h:
 dipswitch.h:
 
 sobel.h:
+
+c:\users\nicis\hesso\msc_ele\tsm_embhardw\labs\lab_04\mse_demo\software\sobel_bsp\hal\inc\sys\alt_timestamp.h:
+
+c:\users\nicis\hesso\msc_ele\tsm_embhardw\labs\lab_04\mse_demo\software\sobel_bsp\hal\inc\alt_types.h:
+
+c:\users\nicis\hesso\msc_ele\tsm_embhardw\labs\lab_04\mse_demo\software\sobel_bsp\drivers\inc\altera_avalon_timer.h:
+
+c:\users\nicis\hesso\msc_ele\tsm_embhardw\labs\lab_04\mse_demo\software\sobel_bsp\hal\inc\sys\alt_dev.h:
+
+c:\users\nicis\hesso\msc_ele\tsm_embhardw\labs\lab_04\mse_demo\software\sobel_bsp\hal\inc\sys\alt_llist.h:
+
+c:\users\nicis\hesso\msc_ele\tsm_embhardw\labs\lab_04\mse_demo\software\sobel_bsp\hal\inc\priv\alt_dev_llist.h:
+
+c:\users\nicis\hesso\msc_ele\tsm_embhardw\labs\lab_04\mse_demo\software\sobel_bsp\hal\inc\sys\alt_warning.h:
