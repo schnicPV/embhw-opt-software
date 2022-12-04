c51605d6af0e3ee87237ac1df071cfc734c87de5
schnicPV <sch.ncls@outlook.com>
2022-12-04 10:00:38 +0100

    run with 8 kB cache and loop optimized sobel_complete (no sub-images used)

--- sobel_bsp/public.mk
+++ sobel_bsp/public.mk
@@ -157,9 +157,9 @@ SOPC_SYSID_FLAG += --sidp=0x10010d8
 ELF_PATCH_FLAG  += --sidp 0x10010d8
 
 # The SOPC Timestamp 
-# setting SOPC_TIMESTAMP is 1669843811
-SOPC_SYSID_FLAG += --timestamp=1669843811
-ELF_PATCH_FLAG  += --timestamp 1669843811
+# setting SOPC_TIMESTAMP is 1670142665
+SOPC_SYSID_FLAG += --timestamp=1670142665
+ELF_PATCH_FLAG  += --timestamp 1670142665
 
 # Enable JTAG UART driver to recover when host is inactive causing buffer to 
 # full without returning error. Printf will not fail with this recovery. none 
