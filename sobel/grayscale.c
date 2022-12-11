cb09cc77f75928c04427e269f43adefb66649f8f
schnicPV <sch.ncls@outlook.com>
2022-12-11 09:16:52 +0100

    SUCCESSFUL RUN of custom conv_gray => problem was malloc (must be width*height)

--- sobel/grayscale.c
+++ sobel/grayscale.c
@@ -16,15 +16,15 @@ int grayscape_height = 0;
 void conv_grayscale(void *picture,
                 int width,
                 int height) {
-  int k,gray;
-  int kmax = (width*height)>>2;	// x>>2 <=> x/4
+  unsigned int k,gray;
+  unsigned int kmax = width*height;
   unsigned int *pixels = (unsigned int *)picture;// , rgb;
   grayscale_width = width;
   grayscape_height = height;
   if (grayscale_array != NULL)
     free(grayscale_array);
   grayscale_array = (unsigned char *) malloc(kmax);
-  for(k = 0; k<kmax; k++)
+  for(k = 0; k < (kmax>>2); k++)								// x>>2 <=> x/4
   {
 	  gray = ALT_CI_CUSTOM_GRAY_0(pixels[k*2], pixels[k*2+1]);
 	  IOWR_32DIRECT(grayscale_array, k*4, gray);
